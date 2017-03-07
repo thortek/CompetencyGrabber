dca1ba6cbc5ffbc4c2edfe37d40248d0a73684c8
Thor Anderson <ta.anderson@gmail.com>
2017-03-07 14:17:25 -0700

    Initial commit of Thor's messy code

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./dist');
+require('babel-polyfill');
+require('babel-register');
+module.exports = require('./src');
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 const Request = require('request');
 const fs = require('fs');
+const async = require('async');
 const dotenv = require('dotenv');
 
 //import dotenv from 'dotenv';
 dotenv.load();
 
-//function getCompetencies() {
+const sample = { "id": 2, "context_id": 1, "context_type": "Account", "vendor_guid": null, "display_name": "", "title": "Use basic Bash/Terminal commands", "url": "/api/v1/outcomes/2", "can_edit": true, "description": "\u003Cp\u003ELearner is able to use Bash commands to:\u003C/p\u003E\r\n\u003Col\u003E\r\n\u003Cli\u003Elist all files in a directory including hidden files\u003C/li\u003E\r\n\u003Cli\u003Enavigate up and down a directory structure\u003C/li\u003E\r\n\u003Cli\u003Eredirect and pipe output between processes\u003C/li\u003E\r\n\u003Cli\u003Eexecute scripts from the terminal\u003C/li\u003E\r\n\u003Cli\u003Edetermine their network interfaces and IP address\u003C/li\u003E\r\n\u003Cli\u003Eidentify and edit environment and profile (e.g. .bash_profile) settings\u003C/li\u003E\r\n\u003Cli\u003Einstall software packages\u003C/li\u003E\r\n\u003Cli\u003Ewrite and execute a custom script\u003C/li\u003E\r\n\u003C/ol\u003E", "calculation_method": "highest", "points_possible": 5.0, "mastery_points": 3.0, "ratings": [{ "description": "Exceeds Expectations", "points": 5.0 }, { "description": "Meets Expectations", "points": 3.0 }, { "description": "Does Not Meet Expectations", "points": 0.0 }], "assessed": 3 };
+
+function getDescriptions() {
+  fs.readFile('competencies.json', (error, data) => {
+    if (error) throw err;
+    // data.forEach((item) => {
+    //   console.log(item);
+    // });
+    let compArray = JSON.parse(data);
+
+    fs.writeFile('descriptions.json', JSON.stringify(compArray), (err) => {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    })
+  });
+}
+
+function getCompetencies() {
   for (let i = 1; i < 140; i++) {
     Request.get('https://lms.heliotraining.com/api/v1/outcomes/' + i, {
       auth: {
@@ -15,26 +33,89 @@ dotenv.load();
     }).on('error', function (err) {
       console.log(err);
     }).on('data', function (data) {
-      
+
       // fs.appendFile('competencies.json', data + ',\n', err => {
       //   if (err) {
       //     throw err;
       //   }
       // });
-      fs.appendFile('descriptions.json', decodeCharRefs(data.description) + ',\n', err => {
+      let parsedData = JSON.parse(data);
+      fs.appendFile('descriptions.json', parsedData.description + ',\n', err => {
         if (err) {
           console.log("Could not decode characters");
         }
       })
+    }).on('response', function (response) {
+      console.log(response.headers);
     });
   }
+}
 
 function decodeCharRefs(string) {
-    return string
-        .replace(/&#(\d+);/g, function(match, num) {
-            return String.fromCharCode(num);
-        })
-        .replace(/&#x([A-Za-z0-9]+);/g, function(match, num) {
-            return String.fromCharCode(parseInt(num, 16));
-        });
+  return string
+    .replace(/&#(\d+);/g, function (match, num) {
+      return String.fromCharCode(num);
+    })
+    .replace(/&#x([A-Za-z0-9]+);/g, function (match, num) {
+      return String.fromCharCode(parseInt(num, 16));
+    });
 }
+
+function getFullStackCompetencyGroups() {
+  //request('http://google.com/doodle.png').pipe(fs.createWriteStream('doodle.png'))
+  Request.get('https://lms.heliotraining.com/api/v1/accounts/1/outcome_groups/6/subgroups?per_page=100', {
+    auth: {
+      bearer: process.env.CANVAS_API_KEY
+    }
+  }).on('error', function (err) {
+    console.log(err);
+  }).pipe(fs.createWriteStream(__dirname + '/data/fullStackCompetencyGroups.json')
+    .on('error', err => {
+      return console.log(err);
+    }));
+}
+
+function getGroupCompetencies() {
+  // const getGroupCompetencies = new Promise(function(resolve, reject) {
+  let allCompetencies = [];
+  let groupArray = new Promise(function(resolve, reject) {
+  
+  fs.readFile(`${__dirname}/data/fullStackCompetencyGroups.json`, (err, data) => {
+    if (err) {
+      reject(Error(err.message));
+    }
+    //console.log(JSON.parse(data));
+    resolve(JSON.parse(data));
+  });
+  }).then(function(result) {
+    console.log(result);
+  }, function(err) {
+    console.log(err);
+  });
+  //console.log(groupArray);
+
+  for (let comp in groupArray) {
+    let groupUrl = comp.outcomes_url;
+    console.log(groupUrl);
+    Request.get(`https://lms.heliotraining.com${groupUrl}?per_page=100`, {
+      auth: {
+        bearer: process.env.CANVAS_API_KEY
+      }
+    }).on('error', function (err) {
+      console.log(err);
+    }).on('data', data => {
+      for (let item of data) {
+        //console.log(item);
+        allCompetencies.push(item);
+      }
+    })
+  }
+  console.log(allCompetencies);
+  // fs.appendFile(`${__dirname}/data/competencies.json`, allCompetencies, (err) => {
+  //   if (err) console.log(err);
+  //   console.log("written to file");
+  // });
+}
+
+//getFullStackCompetencyGroups();
+getGroupCompetencies();
